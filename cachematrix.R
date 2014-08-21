f8b8abe372220dcfcc253e6bdfcbe9fd007c16e8
phasedephase <phasedephase@gmail.com>
2014-08-21 14:52:36 -0400

    finish commenting cacheSolve()

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,10 @@
-## The makeCacheMatrix() and cacheSolve() functions work together to compute
+## The cacheSolve() and makeCacheMatrix() functions work together to compute
 ## and cache the inverse of a matrix either initially passed in as an argument
 ## to makeCacheMatrix() or set later by a call to the set() function as defined
 ## in the local scope of makeCacheMatrix().
 ##
-## The cacheSolve() function computesthe inverse of the matrix stored in the
-## makeCacheMatrix closure.
+## The cacheSolve() function computes the inverse of the matrix stored in the
+## makeCacheMatrix() closure.
 
 
 ## Function Name:  makeCacheMatrix
@@ -13,56 +13,67 @@
 ##
 ## OUTPUT: a list object holding references to 4 functions defined in the closure
 ##
-## Description: this function caches the inverse of a matrix and defines 4 
-## functions in its closure or local scope to 
-## 1) set the local variable acting as the cache to NULL when a new matrix object is set
-## 2) get the matrix object that was passed into the function as argument
-## 3) set the inverse of the matrix passed in as argument to the set() function
-## 4) get the cached inverse of the matrix that was passed in by calling makeCacheMatrix() or
-##    or set()
+## Description: Cache the inverse of a matrix and define 4 functions in the
+## closure or local scope in order to
+## 1) set(): set a new matrix and set the local variable acting as the cache to NULL 
+## 2) get(): get the matrix object that was passed into the function as argument
+## 3) setInvMatrix(): set the inverse of the matrix passed in as argument to 
+##                    either makeCacheMatrix() or the set() function
+## 4) getInvMatrix(): get the cached matrix inverse
 
-makeCacheMatrix <- function(mx = matrix()) {
-    inv_matrix  <- NULL
+makeCacheMatrix <- function(mx_init = matrix()) {
+    invmx_cached  <- NULL
     
-    set <- function(new_matrix) {
-        mx <<- new_matrix
-        inv_matrix <<- NULL
+    set <- function(new_mx) {
+        mx_init <<- new_mx
+        invmx_cached <<- NULL
     }
     
-    get <- function() mx
+    get <- function() mx_init
     
-    setInvMatrix <- function(cached_invmx) {
-        inv_matrix <<- cached_invmx
+    setInvMatrix <- function(invmx) {
+        invmx_cached <<- invmx
     }
     
-    getInvMatrix <- function() inv_matrix
+    getInvMatrix <- function() invmx_cached
     
     list(set = set, get = get, setInvMatrix = setInvMatrix, getInvMatrix = getInvMatrix)
 }
 
 
 ## Function Name:  cacheSolve
-## INPUT:
+## INPUT: mx_init - a list object returned by makeCacheMatrix() holding 4 functions
+##        to manipulate a matrix and its inverse
+##        ... - additional args are not used at the moment
 ##
-## OUTPUT:
+## OUTPUT: invmx_cached - the inverse of the matrix initially passed into 
+##         makeCacheMatrix()
 ##
-## Description:
-##
+## Description: Retrieve from the list object passed as arg and made by makeCacheMatrix()
+##              the inverse of the matrix used when first calling makeCacheMatrix().
+##              If cacheSolve() has already been called once, then return the 
+##              matrix inverse that is cached in the closure of the makeCacheMatrix() 
+##              function. If cacheSolve() is called for the first time, then 
+##              there is no inverse matrix cached yet, and we retrieve the initial
+##              matrix passed as an arg to makeCacheMatrix() or set via the  
+##              set() function defined in the closure of makeCacheMatrix(). We
+##              then call solve() on this matrix to get its inverse and cache it
+##              with a call to setInvMatrix() before returning it.
 
-cacheSolve <- function(mx, ...) {
-        ## Return a matrix that is the inverse of 'x'
-    invmx_cached <- mx$getInvMatrix()
+cacheSolve <- function(mx_init, ...) {
+    invmx_cached <- mx_init$getInvMatrix()
     
     if (!is.null(invmx_cached)) {
         message('getting cached data')
         return(invmx_cached)
     }
     
-    mx_cached <- mx$get()
-    invmx_cached <- solve(mx_cached)
-    mx$setInvMatrix(invmx_cached)
+    mx <- mx_init$get()
+    invmx_cached <- solve(mx)
+    mx_init$setInvMatrix(invmx_cached)
     invmx_cached
 }
+
 
 makeVector <- function(x = numeric()) {
     m <- NULL
